a508861505d1be3db7ea819c666212467963ad63
qiyoudaoyi <qiyoudaoyi@gmail.com>
2023-11-09 09:30:41 +0800

    uid and username are now stateful in localstorage

--- pages/login.vue
+++ pages/login.vue
@@ -1,38 +1,40 @@
 <template>
   <NuxtLayout>
-    <form
-      class="m-auto flex flex-col gap-8 rounded-2xl bg-slate-100 p-8"
-      @submit.prevent="handleStart"
-    >
-      <Logo class="m-auto w-1/3 fill-purple-500" />
-      <InputComponent
-        v-model="uid"
-        type="text"
-        title="UID"
-        placeholder="請輸入 UID"
-        :disabled="false"
-        :select-only="false"
-      />
-      <InputComponent
-        v-model="username"
-        type="text"
-        title="使用者名稱"
-        placeholder="請輸入使用者名稱"
-        :disabled="false"
-        :select-only="false"
-      />
-      <div class="flex justify-center gap-8">
-        <CardButton class="rounded-lg bg-blue-500 text-2xl text-white">
-          開始
-        </CardButton>
-        <CardButton
-          class="rounded-lg bg-gray-400 text-2xl text-white"
-          @click.prevent="() => (uid = generateUid())"
-        >
-          自動產生 UID
-        </CardButton>
-      </div>
-    </form>
+    <ClientOnly>
+      <form
+        class="m-auto flex flex-col gap-8 rounded-2xl bg-slate-100 p-8"
+        @submit.prevent="handleStart"
+      >
+        <Logo class="m-auto w-1/3 fill-purple-500" />
+        <InputComponent
+          v-model="uid"
+          type="text"
+          title="UID"
+          placeholder="請輸入 UID"
+          :disabled="false"
+          :select-only="false"
+        />
+        <InputComponent
+          v-model="username"
+          type="text"
+          title="使用者名稱"
+          placeholder="請輸入使用者名稱"
+          :disabled="false"
+          :select-only="false"
+        />
+        <div class="flex justify-center gap-8">
+          <CardButton class="rounded-lg bg-blue-500 text-2xl text-white">
+            開始
+          </CardButton>
+          <CardButton
+            class="rounded-lg bg-gray-400 text-2xl text-white"
+            @click.prevent="() => (uid = generateUid())"
+          >
+            自動產生 UID
+          </CardButton>
+        </div>
+      </form>
+    </ClientOnly>
   </NuxtLayout>
 </template>
 
@@ -80,6 +82,14 @@ const handleStart = async () => {
 
   authStore.setAccessToken(data.value.accessToken);
 
+  localStorage.setItem('uid', uid.value);
+  localStorage.setItem('username', username.value);
+
   router.push('/');
 };
+
+onMounted(() => {
+  uid.value = localStorage.getItem('uid') || '';
+  username.value = localStorage.getItem('username') || '';
+});
 </script>
